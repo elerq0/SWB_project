c424d76335792ac8c7a1fac4447455ab1d8247e6
eleR <elerq0@gmail.com>
2018-06-10 15:16:56 +0200

    v2

---
+++ engine.c
@@ -0,0 +1,53 @@
+#include <REGX52.H>
+#define IN1 P2_1
+#define IN2 P2_2
+#define IN3 P2_5
+#define IN4 P2_6
+#define EN1 P2_3
+#define EN2 P2_4
+
+volatile unsigned char timer1 = 0;
+volatile unsigned char timer2 = 0;
+
+void main(void)
+{
+	EX0 = 1;
+	IT0 = 1;
+	EX1 = 1;
+	IT1 = 1;
+	EA  = 1;
+	
+	IN1 = 0;
+	IN2 = 1;
+	EN1 = 1;
+	EN2 = 1;
+	IN3 = 1;
+	IN4 = 0;
+	
+	P1_7 = 0;
+	
+	while(1){;}
+}
+
+void but(void) interrupt 0
+	 {
+		 timer1 += 1;
+	   if(timer1 > 2)
+		 {
+			 EN1 = !EN1;
+			 IN1 = !IN1;
+			 IN2 = !IN2;
+			 EN1 = !EN1;
+			 timer1 = 0;			 
+		 }
+	 }
+	 
+void but2(void) interrupt 2
+	 {
+	   timer2 += 1;
+		 if(timer2 > 2)
+		 {
+			 P1_7 = !P1_7;
+			 timer2 = 0;
+		 }
+	 }
--- serial.c
+++ serial.c
@@ -1,2 +1,52 @@
 #include <REGX52.H>                    
-  
+volatile unsigned char newValue, oldValue;
+
+unsigned char code tableTranslation[] = { 0x06, 0x5B, 0x4F,
+																					0x66, 0x6D, 0x7D,
+																					0x07, 0x7F, 0x6F,
+																					0x3F, 0x77, 0x71};
+
+
+void SIR_SerialPort(void) interrupt 4
+  {
+  	if (RI == 1)
+	  {
+			oldValue = newValue;
+	  	newValue = SBUF;
+	    RI = 0;
+	  }
+  }
+	
+void main(void){
+  SCON = 0x50;
+	TMOD = 0x20;
+	PCON = 0x80;
+	TH1 = 0xFF;
+	TL1 = 0xFF;
+	TR1 = 1;
+	IE |= 0x90;
+	
+	oldValue = 0;
+	newValue = 0;
+	P2 = 0x00;
+	P2_0 = 1;
+	P2_1 = 0;
+  while(1)
+	{
+	P0 = tableTranslation[newValue];
+	}
+}
+
+void lele(void)
+{
+	
+	P0 = 0x00;
+	P0 = tableTranslation[oldValue];
+	P2_0 = !P2_0;
+	P2_1 = !P2_1;
+	P0 = 0x00;
+	
+	P2_0 = !P2_0;
+	P2_1 = !P2_1;
+	
+}
